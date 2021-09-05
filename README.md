5026286d3242d4cc84b083f37ee10e73b3ef116e
S.M.ABTAHI NOOR <abtahinoorkabid@gmail.com>
2021-09-05 12:39:27 +0600

    Players Rest API

---
+++ README.md
@@ -0,0 +1,16 @@
+# players-rest-api-nodejs
+
+I made a rest api with node js and express js
+
+## 💻 Developed By
+
+![Developer PIC](https://avatars.githubusercontent.com/u/73340940?s=48&v=4)
+
+## 🚀 Connect with me
+
+[![Facebook Badge](https://img.shields.io/badge/Facebook-1877F2?style=for-the-badge&logo=facebook&logoColor=white)](https://facebook.com/abtahinoorsm)
+[![Instagram Badge](https://img.shields.io/badge/Instagram-E4405F?style=for-the-badge&logo=instagram&logoColor=white)](https://instagram.com/smabtahinoor)
+[![Linkedin Badge](https://img.shields.io/badge/LinkedIn-0077B5?style=for-the-badge&logo=linkedin&logoColor=white)](https://linkedin.com/in/smabtahinoor)
+[![Github Badge](https://img.shields.io/badge/GitHub-100000?style=for-the-badge&logo=github&logoColor=white)](https://github.com/19smabtahinoor)
+[![Mail Badge](https://img.shields.io/badge/Gmail-D14836?style=for-the-badge&logo=gmail&logoColor=white)](mailto:abtahinorkabid@gmail.com)
+[![Discord Badge](https://img.shields.io/badge/Discord-7289DA?style=for-the-badge&logo=discord&logoColor=white)](https://discord.gg/WJjCBB86PJ)
---
+++ index.js
@@ -0,0 +1,79 @@
+const express = require('express');
+const app = express();
+
+app.use(express.json());
+
+const players = [
+    { id: 1, name: "Tamim Iqbal", born: "March 20, 1989", age: 32 },
+    { id: 2, name: "Liton Das", born: "February 16, 1994", age: 27 },
+    { id: 3, name: "Soumya Sarkar", born: "February 25, 1993", age: 28 },
+    { id: 4, name: "Shakib Al Hasan", born: "March 24, 1987", age: 34 },
+    { id: 5, name: "Mushfiqur Rahim", born: "May 9, 1987", age: 34 },
+    { id: 6, name: "Mahmudullah Riyad", born: " February 4, 1986", age: 35 },
+    { id: 7, name: "Afif Hossain", born: "September 22, 1999", age: 21 },
+    { id: 8, name: "Nasum Ahmed", born: " December 5, 1994", age: 26 },
+    { id: 9, name: "Saifuddin", born: "November 1, 1996", age: 24 },
+    { id: 10, name: "Taskin Ahmed", born: " April 3, 1995", age: 26 },
+    { id: 11, name: "Mustafizur Rahman", born: "September 6, 1995", age: 25 },
+]
+
+//✅get 
+app.get('/', (req, res) => {
+    res.send('<h1>Welcome to my players api. Go to <a href="/api/players">Players API</a></h1>');
+})
+
+app.get('/api/players', (req, res) => {
+    res.send(players);
+})
+
+app.get('/api/players/:id', (req, res) => {
+    const player = players.find(item => item.id === parseInt(req.params.id));
+
+    if (!player) {
+        return res.status(404).send('404.Given id was not found!!')
+    }
+    res.send(player);
+})
+
+//✅post
+app.post('/api/players', (req, res) => {
+    const player = {
+        id: players.length + 1,
+        name: req.body.name,
+        born: req.body.born,
+        age: req.body.age,
+    }
+    players.push(player);
+    res.send(player);
+})
+
+//✅ Put or update
+app.put('/api/players/:id', (req, res) => {
+    const player = players.find(item => item.id === parseInt(req.params.id));
+    player.name = req.body.name;
+    res.send(player);
+})
+
+
+//✅ Delete
+app.delete('/api/players/:id' , (req, res) => {
+    const player = players.find(item => item.id === parseInt(req.params.id));
+    players.splice(player, 1);
+    res.send(player);
+})
+
+
+
+
+
+
+
+
+
+
+
+
+
+//create server 
+const port = process.env.PORT || 3000
+app.listen(port, console.log(`Server listening on http://localhost:${port}`))
